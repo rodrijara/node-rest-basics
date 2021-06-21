6c67694f98bc70dc0d4cacd876ed5e717ecffa4d
Rodrigo Jara <rodrigoagustinjara@gmail.com>
2021-06-21 18:49:18 +0200

    update gitignore

--- api/routes/products.js
+++ api/routes/products.js
@@ -27,5 +27,33 @@ router.get('/:productID', (req, res, next) => {
     }
 })
 
+router.patch('/:productID', (req, res, next) => {
+    const id = req.params.productID;
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'you have updated the special ID', 
+            id: id 
+        })
+    }else{
+        res.status(200).json({
+            message: 'You did not passed the special ID'
+        })
+    }
+})
+
+router.delete('/:productID', (req, res, next) => {
+    const id = req.params.productID;
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'you have deleted the special ID', 
+            id: id 
+        })
+    }else{
+        res.status(200).json({
+            message: 'You did not passed the special ID'
+        })
+    }
+})
+
 module.exports = router;
 
